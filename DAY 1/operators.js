a3a4550201929faa2a69009d3c3c4a90a874b0dd
SyedAnwarAli08 <118413194+SyedAnwarAli08@users.noreply.github.com>
2022-11-22 12:04:34 +0530

    updating

---
+++ DAY 1/operators.js
@@ -0,0 +1,24 @@
+//JS is loosely types i.e there is no need to privide the data type
+//declaring a variable
+var a//declaration a variable, now if ou try to print it the output will be "undefined"
+a=10 //initializing a variable
+console.log(a)
+console.log(typeof (a))//this is used to find the data type of a variable
+a="Anwar"
+//now the tyoeof value of a is changed from number to String
+console.log(typeof (a))
+//Rules for variable name
+/*
+Valid names:
+Lower Case
+Upper Case
+Numericals (0-9)
+Sybols (_,$)
+Invalid name:
+can not have spaces in between
+Keywords or reserved words(like if for etc)
+Whitespace
+Can not start with a number
+SPECIAL CHARACTERS EXCEPT (_,$)
+
+*/
---
+++ DAY 1/print.js
@@ -0,0 +1,20 @@
+//comment
+//console.log("") this is used to print output
+//semi colon ; is not necessary in javascript
+ 
+console.log("Hello boi")
+//cosole log adds a new line by default at the end
+//here "," is used to add seperate things in console.log log
+//to print """ use "/"" now the output will be """
+//to print "/" use "//" now the output will be "/"
+console.log("Syed Anwar","rizwan")
+console.log("Welcome to JS!")
+console.log("Adnan \"ali\" Khan")
+console.log("5/10= 0.5")
+console.log("This is 'JS'")
+console.log("Hi,How are you?")
+console.log(":\\\\\\")
+
+// \n this used for new line
+//You can also use '' or `` instead of ""
+// \t this is for tab space
