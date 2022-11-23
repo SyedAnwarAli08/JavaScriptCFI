cce2523aaac0b27cce9a99cbf753b353568cd98e
SyedAnwarAli08 <118413194+SyedAnwarAli08@users.noreply.github.com>
2022-11-23 11:16:11 +0530

    Day 2

---
+++ DAY 2/arithmeticOperators.js
@@ -0,0 +1,11 @@
+/*
+Different Types of arithmetic operators:
+
+1. +
+2. -
+3. *
+4. /
+5. %
+6. ** Example 2**3 = 8
+*/
+console.log()
---
+++ DAY 2/comparisionOperator.js
@@ -0,0 +1,11 @@
+// (===) strictly equal to
+console.log("100"===100)
+//here the answer is false because if the data 
+//type and the value is same then only it will return true
+console.log("100"==100)
+//Here the answer is true because it is checking only the value not 
+//the data type
+//All the comparision operators except strictly equal to (===) compare only 
+//value not the data type
+var a="ball",b=10;
+console.log(a<b)
